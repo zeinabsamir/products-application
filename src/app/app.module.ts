e8d7fe1fc565b02fdef92007e64fee1f4a2607d7
zeinab <zeinabsamir8@gmail.com>
2018-10-18 02:47:18 +0200

    Make a form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
@@ -14,7 +15,8 @@ import { ProductCreateComponent } from './product/product-create/product-create.
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product-create/product-create.component.ts
+++ src/app/product/product-create/product-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
@@ -10,6 +11,20 @@ export class ProductCreateComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  onCreateProduct(form: NgForm) {
+     if (form.invalid) {
+         return;
+     }
+    const product = {
+      SKU: form.value.sku,
+      name: form.value.name,
+      category: form.value.category,
+      price: form.value.price,
+      imageUrl: form.value.image
+
+    };
+    console.log(product);
   }
 
 }
